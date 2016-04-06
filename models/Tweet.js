39b5e721d7f40c7275cec48f738246543124e205
scottdomes <scottdomes@gmail.com>
2016-04-06 15:30:56 -0700

    Added Tweet model

---
+++ models/Tweet.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  twid : String,
+  active : Boolean,
+  author : String,
+  avator : String,
+  body : String,
+  date : Date,
+  screename: String
+});
+
+// Fetches tweet data from the db
+schema.statics.getTweets = function(page, skip, callback) {
+
+  var tweets = [];
+  // var start = (page * 10) + (skip * 1);
+
+  Tweet.find({}, 'twid active author avatar body data screenname').sort({date: 'desc'}).exec(function(err, docs) {
+
+    if (!err) {
+      tweets = docs;
+      tweets.forEach(function(tweet) {
+        tweet.active = true;
+      });
+    }
+
+    callback(tweets); // Pass them to callback
+  });
+
+};
+
+module.exports = Tweet = mongoose.model('Tweet', schema);
--- utils/streamHandler.js
+++ utils/streamHandler.js
@@ -1,5 +1,17 @@
+var Tweet = require('../models/Tweet');
+
 module.exports = function(stream, io) {
   stream.on('data', function(data) {
-    console.log(data);
+
+    var tweet = {
+      twid: data.id,
+      active: false,
+      author: data.user.name,
+      avatar: data.user.profile_image_url,
+      body: data.text,
+      date: data.created_at,
+      screenname: data.user.screen_name
+    };
+    console.log(tweet);
   });
 };
