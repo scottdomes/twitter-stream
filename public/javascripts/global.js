369860b8ad7e3537b66105738f254089070503fc
scottdomes <scottdomes@gmail.com>
2016-04-07 14:27:03 -0700

    Added heading to let user know when stream is real time

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,19 +8,24 @@ $(function() {
   var tweetSpeed = $('#tweet-speed');
 
   $('#faster').on('click', function() {
-    if (tweetDelay <= 250) {
+    if (tweetDelay <= 250 && tweetDelay > 50) {
       tweetDelay -= 50;
       tweetSpeed.text(tweetDelay / 1000);
-    } else if (tweetDelay <= 100) {
-      console.log("Zero!");
-      tweetSpeed.text(tweetDelay / 1000);
-    } else {
+    } else if (tweetDelay === 50) {
+      tweetDelay -= 50;
+      $('#tweet-counter').hide();
+      $('#realtime').css('display', 'inline-block');
+    } else if (tweetDelay > 250) {
       tweetDelay -= 250;
       tweetSpeed.text(tweetDelay / 1000);
     }
   });
 
   $('#slower').on('click', function() {
+    if (tweetDelay === 0) {
+      $('#realtime').hide();
+      $('#tweet-counter').css('display', 'inline-block');
+    }
     tweetDelay += 250;
     tweetSpeed.text(tweetDelay / 1000);
   });
