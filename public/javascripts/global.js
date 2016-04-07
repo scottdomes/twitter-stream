90714c47e2fba80ea99cfebb481bc0eace745881
scottdomes <scottdomes@gmail.com>
2016-04-06 18:31:52 -0700

    Added variable interval time between tweets

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,6 +2,8 @@ $(function() {
 
   var socket = io.connect('http://localhost:3000');
   var tweetsArray = [];
+  var delay = 2000;
+  
   socket.on('tweet', function(tweet) {
       tweetsArray.unshift(tweet);
   });
@@ -9,6 +11,6 @@ $(function() {
   setInterval(function() {
     tweetHTML = "<p>" + tweetsArray.shift().body + "</p>";
     $('#tweets').prepend(tweetHTML);
-  }, 2000);
+  }, delay);
 
 });
