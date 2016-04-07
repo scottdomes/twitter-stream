6d3a2ae461813be592b4ea3ac3b521e31cfd132e
scottdomes <scottdomes@gmail.com>
2016-04-06 18:41:41 -0700

    Added faster and slower buttons for controlling stream speed

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,14 +3,34 @@ $(function() {
   var socket = io.connect('http://localhost:3000');
   var tweetsArray = [];
   var delay = 2000;
-  
+
+  $('#faster').on('click', function() {
+    if (delay > 4000) {
+      delay -= 2000;
+    } else if (delay <= 200) {
+      console.log("Zero!");
+    } else {
+      delay -= 500;
+    }
+  });
+
+  $('#slower').on('click', function() {
+    delay += 2000;
+    console.log(delay);
+  });
+
   socket.on('tweet', function(tweet) {
       tweetsArray.unshift(tweet);
   });
 
-  setInterval(function() {
-    tweetHTML = "<p>" + tweetsArray.shift().body + "</p>";
-    $('#tweets').prepend(tweetHTML);
-  }, delay);
+  var timeout = function() {
+    setTimeout(function() {
+      tweetHTML = "<p>" + tweetsArray.shift().body + "</p>";
+      $('#tweets').prepend(tweetHTML);
+      timeout();
+    }, delay);
+  };
+
+  timeout();
 
 });
